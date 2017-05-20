566a0b16ce383482efa260bea58c89cf24259195
yhzhuo <yaohuazhuo@gmail.com>
2017-05-20 16:11:35 -0700

    add fake data to tasks ranking sample

--- java/TasksRanking/src/self/yaohua/taskranking/Goal.java
+++ java/TasksRanking/src/self/yaohua/taskranking/Goal.java
@@ -15,7 +15,14 @@ public class Goal {
 	
 	// Ids of all goals.
 	
+	// 3.2
+	static final String haveGoodCareer = "haveGoodCareer";
+	static final String gettingMarried = "gettingMarried";
+	static final String formStableAndSolidSocialGroups = "formStableAndSolidSocialGroups";
+	
 	static final Map<String, Goal> goals = ImmutableMap.<String, Goal>builder()
-
-		.build();
+		.put(haveGoodCareer, new Goal(10.0, "As a new employee I plan to stay at my company for a while, working on interesting projects, and getting promoted."))
+		.put(gettingMarried, new Goal(9.0, "Getting married with a good girl and prepare to be a good husband and father in future."))
+		.put(formStableAndSolidSocialGroups, new Goal(5.0, "Meeting with different people, know there thoughts, and form stable and solid social groups."))
+	.build();
 }
--- java/TasksRanking/src/self/yaohua/taskranking/Task.java
+++ java/TasksRanking/src/self/yaohua/taskranking/Task.java
@@ -19,8 +19,49 @@ public class Task {
 	
 	// Ids of all tasks.
 	
+	static final String ensureQualityAndSpeedOfEachTask = "ensureQualityAndSpeedOfEachTask";
+	static final String talkMoreWithTeamMembersAboutTheirTasks = "talkMoreWithTeamMembersAboutTheirTasks";
+	static final String dateGirls = "dateGirls";
+	static final String learnCooking = "learnCooking";
+	static final String attendSocialActivities = "attendSocialActivities";
+	static final String alwaysReadSocialInformation = "alwaysReadSocialInformation";
+	
 	// Description of all tasks.
 	static final Map<String, Task> tasks = ImmutableMap.<String, Task>builder()
+			.put(ensureQualityAndSpeedOfEachTask, new Task(3.0, ImmutableMap.<String, Double>builder()
+					.put(Goal.haveGoodCareer, 0.95)
+					.put(Goal.formStableAndSolidSocialGroups, 0.2)
+				.build(),
+				"Ensure the quality, quantity, and speed of each tasks you finish, and never rush."))
 			
+			.put(talkMoreWithTeamMembersAboutTheirTasks, new Task(1.75, ImmutableMap.<String, Double>builder()
+					.put(Goal.haveGoodCareer, 0.5)
+					.put(Goal.formStableAndSolidSocialGroups, 0.75)
+				.build(),
+				"Talk with others about there tasks, especially the ones collaborate closely with you. Know scope of each tasks and each ones tasks, know each one's responsibility, communicate first before working on other's responsibilities."))
+			
+			.put(dateGirls, new Task(2.75, ImmutableMap.<String, Double>builder()
+					.put(Goal.gettingMarried, 0.95)
+					.put(Goal.formStableAndSolidSocialGroups, 0.55)
+				.build(),
+				"Date girls with the goal of getting married. Try to learn from them and know their ideas."))
+			
+			.put(learnCooking, new Task(2.0, ImmutableMap.<String, Double>builder()
+					.put(Goal.gettingMarried, 0.55)
+					.put(Goal.formStableAndSolidSocialGroups, 0.2)
+				.build(),
+				"Learn to cook good, nice, and delicious food."))
+			
+			.put(attendSocialActivities, new Task(2.75, ImmutableMap.<String, Double>builder()
+					.put(Goal.gettingMarried, 0.45)
+					.put(Goal.formStableAndSolidSocialGroups, 0.95)
+				.build(),
+				"Attend off line social activities received from various sources."))
+				
+			.put(alwaysReadSocialInformation, new Task(2.75, ImmutableMap.<String, Double>builder()
+					.put(Goal.gettingMarried, 0.15)
+					.put(Goal.formStableAndSolidSocialGroups, 0.55)
+				.build(),
+				"Form the habit of always read social related information various channels, and extract valuable information relate to social activities."))
 		.build();
 }
